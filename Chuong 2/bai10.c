89911bd36cc805c78d6b77b435067bcb1c0c6b28
Lam Hoang Tung <lamhoangtung@Lams-iMac.local>
2018-04-13 09:17:37 +0700

    Fix some bugs

--- Chuong 2/bai10.c
+++ Chuong 2/bai10.c
@@ -3,10 +3,16 @@
 int main(void){
 	int n;
 	printf("Nhap n: ");scanf("%i",&n);
+	int m=n;
 	printf("a) n++ + 3 = %i\n",n+4);
+	n=m;
 	printf("b) ++n + 3 = %i\n",n+4);
+	n=m;
 	printf("c) n++ + ++n = %i\n",n+n+2);
+	n=m;
 	printf("d) --n + --n = %i\n",n+n-2);
+	n=m;
 	printf("e) n/2 = %i\n",n/2);
+	n=m;
 	printf("f) n%2 = %i\n",n%2);
 }
