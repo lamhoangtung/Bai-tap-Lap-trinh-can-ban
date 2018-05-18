155d2c5a79b151a346b20991091b10c2f1859106
lamhoangtung <lamhoangtung.vz@gmail.com>
2018-05-18 08:10:11 +0700

    Perfection some of the code ;)

--- Chuong 5/bai2.c
+++ Chuong 5/bai2.c
@@ -2,25 +2,35 @@
 
 int hoanhao(int n){
   int tong=0;
+
   for (int i=1;i<n;i++){
-    if (n%i==0) tong+=i;
+    if (n%i==0){
+      tong+=i;
+    }
   }
-  if (tong!=n) return 0;
-  else return 1;
+  if (tong!=n){
+    return 0;
+  }
+  else{
+    return 1;
+  }
 }
 
-int main(void){
+int main(){
   int n,a[10000];
+
   printf("Nhap so phan tu cua mang: ");
   scanf("%i",&n);
   for (int i=1;i<=n;i++){
     printf("Nhap phan tu thu %i cua mang: ",i);
     scanf("%i",&a[i]);
   }
+
   printf("\nMang ban vua nhap la:\n");
   for (int i=1;i<=n;i++){
     printf("%i ",a[i]);
   }
+
   long long sum=0;
   printf("\n\nCac so hoan hao co trong mang la: \n");
   for (int i=1;i<=n;i++){
@@ -30,4 +40,6 @@ int main(void){
     }
   }
   printf("\nTong cac so hoan hao trong mang la %lld\n",sum);
+
+  return 0;
 }
