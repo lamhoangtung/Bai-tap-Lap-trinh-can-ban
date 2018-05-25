748d4f55bc9c11c983446cd35fdf2d9bbd58423d
lamhoangtung <lamhoangtung.vz@gmail.com>
2018-05-25 15:50:46 +0700

    Completed first version of searchContact() function

--- Bai tap lon/main.c
+++ Bai tap lon/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 #include <string.h>
+#include <ctype.h>
 
 struct contact{
   char name[30];
@@ -24,6 +25,38 @@ int countline(){
   return lines;
 }
 
+int check_subsequence(char a[], char b[]){
+  int c,d;
+  c=d=0;
+  while (a[c]!='\0'){
+    while ((a[c]!=b[d]) && b[d]!='\0'){
+      d++;
+    }
+    if (b[d]=='\0') break;
+    d++;
+    c++;
+  }
+  if (a[c]=='\0') return 1;
+  else return 0;
+}
+
+void printContact(int id, char data[]){
+  int begin=0,end;
+  printf("\t\t%i\t",id);
+  for (int i=0;i<strlen(data);i++){
+    if (data[i]=='|'){
+      end=i-1;
+      for (int j=begin;j<=end;j++){
+        printf("%c",data[j]);
+      }
+      begin=i+1;
+      printf("\t\t");
+    }
+  }
+  end=strlen(data)-1;
+  for (int j=begin;j<=end;j++) printf("%c",data[j]);
+  printf("\n");
+}
 
 void addContact(){
   temp = (struct contact) {"",0,""};
@@ -40,36 +73,44 @@ void addContact(){
   printf("\n\t\tAdded %s to the contact book.",temp.name);
 }
 
-
 void listContact(){
   int lines=countline();
   if (lines==0) printf("\n\t\tThere is no contact to display.");
   else {
     char savedData[100];
-    int id=0;
     printf("\n\t\tThere are %i contacts in the contact book:\n\n",lines);
     database = fopen("database.txt","r");
-    while(id<lines){
+    for (int id=1;id<=lines;id++){
       fscanf(database," %[^\n]%*c",savedData);
       //printf("\t\t%s\n",savedData);
-      int begin=0,end;
-      id++;
-      printf("\t\t%i\t",id);
-      for (int i=0;i<strlen(savedData);i++){
-        if (savedData[i]=='|'){
-          end=i-1;
-          for (int j=begin;j<=end;j++){
-            printf("%c",savedData[j]);
-          }
-          begin=i+1;
-          printf("\t\t");
-        }
-      }
-      end=strlen(savedData)-1;
-      for (int j=begin;j<=end;j++) printf("%c",savedData[j]);
-      printf("\n");
+      printContact(id,savedData);
     }
     fclose(database);
+  }
+}
+
+void searchContact(){
+  printf("\n\n\t\tPlease type in something to search: ");
+  char query[30];
+  scanf(" %[^\n]%*c",query);
+  for (int i=0;i<strlen(query);i++){
+    query[i]=tolower(query[i]);
+  }
+  //printf("%s\n",query);
+  int lines=countline();
+  database = fopen("database.txt","r");
+  for (int id=1;id<=lines;id++){
+    int flag;
+    char savedData[100];
+    fscanf(database," %[^\n]%*c",savedData);
+    char preprocessedData[100];
+    for (int i=0;i<strlen(savedData);i++){
+      if (savedData[i]=='|'){
+        preprocessedData[i]=' ';
+      } else preprocessedData[i]=tolower(savedData[i]);
+    }
+    //printf("%s\n",preprocessedData);
+    if (strstr(preprocessedData,query)!=NULL) printContact(id,savedData);
   }
 }
 
@@ -87,7 +128,7 @@ int main(){
   printf("\n\t **** Welcome to T3-KHMT1-K12 Contact Manager ****");
   while (1){
     printf("\n\n\n\t\t\tMAIN MENU\n\t\t=====================\n\t\t[1] Add a new");
-    printf(" Contact\n\t\t[2] List all Contacts\n\t\t[3] Search for contact\n\t\t[4] Edit");
+    printf(" Contact\n\t\t[2] List all Contacts\n\t\t[3] Search for contact\n\t\t[4] Edit");x
     printf(" a Contact\n\t\t[5] Delete a Contact\n\t\t[0] Exit\n\t\t=================\n\t\t");
     printf("Enter the choice: ");
     scanf("%i",&choice);
@@ -115,10 +156,9 @@ int main(){
       case 2: listContact();
               break;
 
-      case 3: printf("\n\n\t\tPlease type in something to search: ");
-              //char query[30];
-              //scanf("%s",&query);
+      case 3: searchContact();
               break;
+              
       case 4: printf("\n\n\t\tPlease type in the ID of the contact: ");
               //int id;
               //scanf("%i",&id);
